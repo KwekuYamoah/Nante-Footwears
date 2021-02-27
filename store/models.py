93b21a1878cb2cf6d6366bc93ff8fc229208e77b
Jeffrey Adorkor <adorkorj@gmail.com>
2021-02-27 20:55:21 +0000

    Dynamically filled pages

--- store/models.py
+++ store/models.py
@@ -1,4 +1,84 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
-
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null = True, blank = True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    AGES = (
+        ('M', 'Men'),
+        ('W', 'Women'),
+        ('K', 'Kids')
+    )
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    age = models.CharField(max_length=1, choices=AGES)
+    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank = True, null = True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank = True, null = True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank = True, null = True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class DeliveryAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank = True, null = True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank = True, null = True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.address
